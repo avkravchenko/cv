22ec23ee989bf3c09160ddc44ca758e10d2da2dc
Alexander <sashakravchenko1996@gmail.com>
2022-12-01 01:13:20 +0600

    making page relative again

--- script/app.js
+++ script/app.js
@@ -1,5 +1,23 @@
 function myF() {
     document.getElementById("abmd").style.display = "block";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("ph").style.display = "none";
+    document.getElementById("photo").style.transform = "translate(0, 0)";
+}
+
+function myS() {
+    document.getElementById("abmd").style.display = "none";
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("ph").style.display = "none";
+    document.getElementById("photo").style.transform = "translate(0, 0)";
+}
+
+function myP() {
+    document.getElementById("ph").style.display = "block";
+    document.getElementById("abmd").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("photo").style.transform = "translate(0, 150px)";
+
 }
 
 console.log('hello');
